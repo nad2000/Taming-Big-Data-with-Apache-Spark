fb23a6015a439531de13917dcc16b40e10d5b193
Rad Cirskis <nad2000@gmail.com>
2016-11-18 00:43:46 +1300

    customer summary...

---
+++ cust-summary.py
@@ -0,0 +1,19 @@
+from pyspark import SparkConf, SparkContext
+from os import path
+import re
+
+DATA_ROOT = "file:///home/nad2000/Dropbox/taming-big-data-with-apache-spark-hands-on"
+
+conf = SparkConf().setMaster("local").setAppName("CustomerSummary")
+sc = SparkContext(conf = conf)
+
+def extract_data(line):
+    fields = line.split(',')
+    return (int(fields[0]), float(fields[2]))
+
+data = sc.textFile(path.join(DATA_ROOT, "customer-orders.csv"))
+result = (data.map(lambda l: extract_data(l))
+        .reduceByKey(lambda x, y: x + y))
+
+for r in result.collect():
+    print "%d\t%.2f" % r
--- word-count.py
+++ word-count.py
@@ -1,17 +1,26 @@
 from pyspark import SparkConf, SparkContext
 from os import path
+import re
+
+normalize_re = re.compile(r'\W+', re.UNICODE)
 
 DATA_ROOT = "file:///home/nad2000/Dropbox/taming-big-data-with-apache-spark-hands-on"
 
 conf = SparkConf().setMaster("local").setAppName("WordCount")
 sc = SparkContext(conf = conf)
 
+def normalize_words(text):
+    return normalize_re.split(text.lower())
+
 input = sc.textFile(path.join(DATA_ROOT, "book.txt"))
 # flatMap maps a collection value into a multiple entries each per an element
-words = input.flatMap(lambda x: x.split())
-wordCounts = words.countByValue()
+words = input.flatMap(normalize_words)
+word_counts = words.map(lambda w: (w, 1)).reduceByKey(lambda x, y: x + y)
+# swap and sort:
+word_counts_sorted = word_counts.map(lambda (x, y): (y, x)).sortByKey()
 
-for word, count in wordCounts.items():
-    cleanWord = word.encode('ascii', 'ignore')
+for rec in word_counts_sorted.collect():
+    count = rec[0]
+    cleanWord = rec[1].encode('ascii', 'ignore')
     if (cleanWord):
-        print(cleanWord.decode() + " " + str(count))
+        print cleanWord, count
